49f9ccc51eb593f86c24d0c1a7eea5f7a55130b0
Zak Golding <zak@0x7cf.com>
2017-03-11 14:52:57 -0800

    Working on open table actions

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer';
+import openTable from './openTableReducer';
 
 const rootReducer = combineReducers({
   auth,
+  openTable,
 });
 
 export default rootReducer;
