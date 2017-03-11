0fad9959e38fd0959f5326db5551604cb9ee9774
astelvida <sevda.m.anefi@gmail.com>
2017-03-11 09:45:57 -0800

    initialize firebase

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAVMguW_OeRcAepB_9ina9qo0qTfaFxjhQ",
+  authDomain: "hackathon-c78f4.firebaseapp.com",
+  databaseURL: "https://hackathon-c78f4.firebaseio.com",
+  storageBucket: "hackathon-c78f4.appspot.com",
+  messagingSenderId: "630454093083"
+};
+
+export default config;
