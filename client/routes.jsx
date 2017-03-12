7a564431ddb515955c2eb9a6d27d6455a02b34af
Sammi A <sammialldredge@gmail.com>
2017-03-12 15:58:54 -0700

    confirmation

--- client/routes.jsx
+++ client/routes.jsx
@@ -4,8 +4,8 @@ import Layout from './components/Layout';
 import LoginForm from './components/LoginForm';
 import Signup from './components/signup/Signup';
 import Home from './components/Home';
+import Confirmation from './components/Confirmation'
 import RequireAuth from './components/RequireAuth';
-
 import PickRestaurant from './components/match/PickRestaurant';
 
 // import Profile from './components/Profile';
@@ -16,22 +16,10 @@ export default (
     <Route path="signup" component={Signup} />
     <Route path="home" component={RequireAuth(Home)} />
     <Route path="listings" component={PickRestaurant} />
-    {/* <Route path="profile/:id" component={Profile} /> */}
-    {/* <Route path="matches" component={Matches} >
-      <Route path="time" component={Time} />
-      <Route path="location" component={Location} />
-      <Route path="confirmation" component={Confirmation} />
-    </Route> */}
+    {/* <Route path="profile/:id" component={Profile} />  */}
+    {/* <Route path="matches" component={Matches} > */}
+    {/* <Route path="time" component={Time} /> */}
+    {/* <Route path="location" component={Location} /> */}
+    <Route path="confirmation" component={Confirmation} />
   </Route>
-);
-
-// =======
-// import AppContainer from './components/AppContainer';
-// import LoginForm from './components/LoginForm';
-// import Setup from './components/Setup';
-//
-// export default (
-//   <Route path="/" component={AppContainer}>
-//     <Route path="login" component={LoginForm} />
-//     <Route path="setup" component={RequireAuth(Setup)} />
-//     <Route path="home" component={RequireAuth(Home)} />
+    );
