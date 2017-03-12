e856b1f2e9f1eae1ed75ce7a4eeebed1c59efbe0
Romain DEJEAN <romain.dejean@gadz.org>
2017-03-12 09:13:54 -0700

    Fixed typo

--- client/components/ProjectOwner.jsx
+++ client/components/ProjectOwner.jsx
@@ -14,17 +14,12 @@ class ProjectOwner extends Component {
     super(props);
     const { location, industry, round, range, product } = this.props;
     this.state = {
+      role: { Investor: false, 'Project Owner': true },
       location,
       industry,
       round,
       range,
       product,
-      // role: { Investor: false, 'Project Owner': true },
-      // location: '',
-      // industry: { Healthcare: false, FinTech: false, Consumer: false, 'Digital Media': false, Ecommerce: false, SaaS: false },
-      // round: { Idea: false, Seed: false, 'Series A': false, 'Series B': false, After: false },
-      // range: { '<100k': false, '100k-300k': false, '300k-500k': false, '500k-1M': false, '>1M': false },
-      // product: { Title: 'Hello', Description: 'Yo' },
     };
     this.handleChangeLocation = this.handleChangeLocation.bind(this);
     this.handleClickRound = this.handleClickRound.bind(this);
