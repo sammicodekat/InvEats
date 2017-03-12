39b75c126ba8448e23ffa7e4974050b5cc6303f7
zsnydr <zackwsnyder@gmail.com>
2017-03-11 23:03:44 -0800

    [delete] remove availability and cuisine from sign up local state

--- client/components/signup/Signup.jsx
+++ client/components/signup/Signup.jsx
@@ -14,14 +14,12 @@ class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      role: { Investor: false, 'Project Owner': false },
       step: 1,
+      role: { Investor: false, 'Project Owner': false },
       location: '',
       industry: { Healthcare: false, FinTech: false, Consumer: false, 'Digital Media': false, Ecommerce: false, SaaS: false },
       round: { Idea: false, Seed: false, 'Series A': false, 'Series B': false, After: false },
       range: { '<100k': false, '100k-300k': false, '300k-500k': false, '500k-1M': false, '>1M': false },
-      cuisine: { Mexican: false, Thai: false, American: false, Italian: false, Japanese: false },
-      schedule: { Monday: { Lunch: false, Dinner: false }, Tuesday: { Lunch: false, Dinner: false }, Wednesday: { Lunch: false, Dinner: false }, Thursday: { Lunch: false, Dinner: false }, Friday: { Lunch: false, Dinner: false } },
       product: { Title: '', Description: '' },
     };
 
