19f56449d22f6da6ec57d1bc7dd62c868dcef5aa
astelvida <sevda.m.anefi@gmail.com>
2017-03-11 22:32:10 -0800

    add classic auth

--- client/store/actions/auth/authActions.js
+++ client/store/actions/auth/authActions.js
@@ -1,9 +1,13 @@
+import firebase from 'firebase';
 import {
-  GOOGLE_LOGIN,
   GOOGLE_LOGIN_SUCCESS,
   GOOGLE_LOGIN_ERROR,
+  LOGIN_USER_START,
+  LOGIN_USER_ERROR,
+  LOGIN_USER_SUCCESS,
+  SIGNUP_USER_SUCCESS,
 } from './authActionTypes';
-import firebase from 'firebase';
+
 
 export const googleLogin = () => (dispatch) => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -25,3 +29,43 @@ export const setUser = (user) => {
     user,
   }
 };
+
+export const loginUserStart = (user) => ({
+  type: LOGIN_USER_START,
+  payload: user,
+});
+
+export const loginUserSuccess = (user) => ({
+  type: LOGIN_USER_SUCCESS,
+  payload: user,
+});
+
+export const signupUserSuccess = (user) => ({
+  type: SIGNUP_USER_SUCCESS,
+  payload: user,
+});
+
+
+export const loginUserError = (error) => ({
+  type: LOGIN_USER_ERROR,
+  payload: error
+});
+
+export const loginUser = (user) => (
+  (dispatch) => {
+    dispatch(loginUserStart(user));
+    const { email, password } = user;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((currentUser) =>
+        firebase.database().ref(`/users/${currentUser.uid}`)
+        .on('value', (snapshot) =>
+          dispatch(loginUserSuccess({ ...user, ...snapshot.val() }))
+        ))
+      .catch((err) =>
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          dispatch(signupUserSuccess(user));
+        })
+      )
+      .catch((error) => dispatch(loginUserError(error.message)));
+  });
